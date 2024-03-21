69c98eafc14fad5802a724afbc2a6a817700fec7
jorge-73 <jorge_ap_73@hotmail.com>
2024-03-20 23:01:45 -0300

    movies app v1.0

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -6,15 +6,19 @@ const LandingPage = () => {
       <h2 className="text-4xl md:text-6xl text-white font-bold text-center md:text-left">
         Get curated show and movie recommendations with Open AI
       </h2>
-      <div className="flex flex-col md:flex-row justify-center md:justify-start items-center gap-3 md:gap-5 mt-5">
-        <Link href={"/movies"}>
-          <a className="btn btn-secondary rounded-full font-bold px-6 md:px-10 py-3 md:py-4 text-base md:text-lg opacity-80">View Movies</a>
+      <div className="flex justify-center gap-5 mt-5">
+        <Link
+          href={"/movies"}
+          className="btn btn-secondary rounded-full font-bold px-10 opacity-80"
+        >
+          View Movies
         </Link>
-        <button className="btn btn-neutral rounded-full font-bold px-6 md:px-10 py-3 md:py-4 text-base md:text-lg opacity-80">Ghost</button>
+        <button className="btn btn-neutral rounded-full font-bold px-10 opacity-80">
+          Ghost
+        </button>
       </div>
     </div>
   );
 };
 
 export default LandingPage;
-
