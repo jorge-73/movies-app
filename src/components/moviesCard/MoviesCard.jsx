4c9a823dd85421556aa419e473ed988bc1c83053
jorge-73 <jorge_ap_73@hotmail.com>
2024-03-21 21:08:09 -0300

    add aos + updates

--- src/components/moviesCard/MoviesCard.jsx
+++ src/components/moviesCard/MoviesCard.jsx
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
 import Link from "next/link";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./MoviesCard.css";
 
 const MoviesCard = ({ movie }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   const imgUrl = `https://image.tmdb.org/t/p/w300/${movie.poster_path}`;
+  
   return (
-    <div className="card movieCard text-white">
+    <div
+      className="movieCard"
+      data-aos="fade-zoom-in"
+      data-aos-easing="ease-in-back"
+      data-aos-delay="100"
+      data-aos-duration="2000"
+      data-aos-offset="0"
+    >
       <Link href={`/movies/${movie.id}`}>
-        <img className="movieImg" src={imgUrl} alt={movie.title}/>
-        {/* <div>{movie.title}</div> */}
+        <img className="movieImg" src={imgUrl} alt={movie.title} />
       </Link>
     </div>
   );
