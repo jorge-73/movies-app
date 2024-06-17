42f34718ed867b31bf742e21cab57535adc6640f
jorge-73 <jorge_ap_73@hotmail.com>
2024-06-17 14:01:51 -0300

    add ai chat

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <Toaster position="bottom-center" reverseOrder={false} />
+        <chat-bot platform_id="b4bdb4c2-d290-4cc6-8540-8757a3eb644f" user_id="a8503940-34e6-4dc9-8cf8-abb30377aa8b" chatbot_id="8610b50e-9396-47f4-a3f3-bb3576352325"><a href="https://www.chatsimple.ai/?utm_source=widget&utm_medium=referral">chatsimple</a></chat-bot><script src="https://cdn.chatsimple.ai/chat-bot-loader.js" defer></script>
       </body>
     </html>
   );
